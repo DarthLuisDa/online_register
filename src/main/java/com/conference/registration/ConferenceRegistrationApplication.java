72ee8dc5e9e46154dd14f1d4214e6170a81c98f4
Luis Daniel Vargas Rodríguez <5cvargas.luis@gmail.com>
2025-04-16 09:26:04 -0600

    Se sube el proyecto base ya con su configuración DB

---
+++ src/main/java/com/conference/registration/ConferenceRegistrationApplication.java
@@ -0,0 +1,13 @@
+package com.conference.registration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConferenceRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConferenceRegistrationApplication.class, args);
+	}
+
+}
